58676e9dc0ed27fd596f22230b644621601dcddc
Hugh <hugh.feary@gmail.com>
2023-11-20 17:12:54 +1100

    working chat bot, start of weather functionality

--- test.py
+++ test.py
@@ -1,6 +1,36 @@
+from flask import *
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+import pandas as pd
+import requests
 
+file = open('API_keys.txt', 'r')
+read = file.readlines()
+file.close()
+API_dict = json.loads(read[0])
+
+app = Flask(__name__)
+app.secret_key="12345"
+app.secret_key = API_dict['flask_secret_key']
+openweather_API_key = API_dict['openweather_API_key']
+google_API_key = API_dict['google_API_key']
+
+itinerary_destinations = {
+        'Lake District National Park': {'latitude': 54.4609, 'longitude': -3.0886},
+        'Corfe Castle': {'latitude': 50.6395, 'longitude': -2.0566},
+        'The Cotswolds': {'latitude': 51.8330, 'longitude': -1.8433},
+        'Cambridge': {'latitude': 52.2053, 'longitude': 0.1218},
+        'Bristol': {'latitude': 51.4545, 'longitude': -2.5879},
+        'Oxford': {'latitude': 51.7520, 'longitude': -1.257},
+        'Norwich': {'latitude': 52.6309, 'longitude': 1.2974},
+        'Stonehenge': {'latitude': 51.1789, 'longitude': -1.8262},
+        'Watergate Bay': {'latitude': 50.4429, 'longitude': -5.0553},
+        'Birmingham': {'latitude': 52.4862, 'longitude': -1.8904}
+    }
+
+
+def get_coords(location):
+    return itinerary_destinations[location]["latitude"], itinerary_destinations[location]["longitude"]
 
 my_bot = ChatBot(
     name="PyBot",
@@ -9,45 +39,57 @@ my_bot = ChatBot(
                     "chatterbot.logic.BestMatch"]
     )
 
-weather_talk = [
-    "Can you please get me the current weather?",
-    "Sure, which location would you like?",
-    "Can I please see the weather forecast?",
-    "Sure, would you like the current weather or a 7 day forecast?"
-]
-
-small_talk = [
-    "Hello",
-    "Hi there!",
-    "How are you doing?",
-    "I'm doing great.",
-    "That is good to hear",
-    "Thank you.",
-    "You're welcome."
-]
-
-math_talk_1 = [
-    'pythagorean theorem',
-    'a squared plus b squared equals c squared.'
-]
-
-math_talk_2 = [
-    'law of cosines',
-    'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)'
-]
-
+weather_dialog = pd.read_csv('forecast_weather.csv')
+# make csv file a flat list to be taken by the list trainer
+q_list = weather_dialog[['question', 'answer']].values.flatten().tolist()
 list_trainer = ListTrainer(my_bot)
+list_trainer.train(q_list)
 
-for item in (small_talk, math_talk_1, math_talk_2, weather_talk):
-    list_trainer.train(item)
+#this needs to be multiple things long otherwise it will incorrectly train
+# for item in (q_list):
+#     list_trainer.train(item)
 
 corpus_trainer = ChatterBotCorpusTrainer(my_bot)
 corpus_trainer.train('chatterbot.corpus.english')
 
-while True:
-    try:
-        user_input = input("You:" )
-        bot_response = my_bot.get_response(user_input)
-        print(f'{my_bot.name}: {bot_response}')
-    except (KeyboardInterrupt, EOFError, SystemExit):
-        break
+
+
+@app.route('/', methods=["POST","GET"])
+def index():
+    if 'conversation' not in session:
+        session['conversation'] = []
+
+    if request.method == 'POST':
+        user_input = request.form.get('user_input')
+        bot_response = my_bot.get_response(user_input).text
+
+        if bot_response == 'No problem I will retrieve that data now':
+            session['location'] = 'Bristol'
+            session['conversation'].extend([user_input, bot_response])
+            session['conversation'] = session['conversation']
+            return redirect(url_for('display_weather'))
+
+
+
+        session['conversation'].extend([user_input, bot_response])
+        session['conversation'] = session['conversation']
+        return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/get_weather',methods=['get','post'])
+def display_weather():
+    lat, long = get_coords(session['location'])
+    print(lat,long)
+    weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={openweather_API_key}"
+    response = requests.get(weather_url)
+    print(response.json())
+    return render_template('weather_results.html', result=response.json())
+
+
+@app.route('/reset',methods=['post']) #route that handles the reset request
+def reset_chat():
+    session['conversation'] = []
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
