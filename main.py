eeda2ca988e45ee0020d17252b569cb2a0ae7080
Hugh <hugh.feary@gmail.com>
2023-11-27 23:16:28 +1100

    refactoring html page and class

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import os
 from bot_trainer import itinerary_trainer, help_trainer
 from data import itinerary_destinations, weather_advice
 
+app = Flask(__name__) # initialisation of flask app
+
 #read text file with API keys assign to variables
 file = open('API_keys.txt', 'r')
 read = file.readlines()
 file.close()
 API_dict = json.loads(read[0])
-
-app = Flask(__name__) # initialisation of flask app
 
 app.secret_key = API_dict['flask_secret_key']
 openweather_API_key = API_dict['openweather_API_key']
@@ -26,10 +26,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) #linking flask app and SQLAlchemy
 
 # class to define structure of the database table
-class Conversation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String, nullable=False) 
-    answer = db.Column(db.String, nullable=False)
+
+
+#class to encapsulate location information, weather data and handle some data processing
 
 # read all of the csv files and add data to conversation table
 def import_data():
@@ -57,6 +56,50 @@ def convert_dt(unix, loc="t"):
         return datetime.utcfromtimestamp(unix).strftime('%a %e %b, %I:%M%p')
     else:
         return datetime.utcfromtimestamp(unix).strftime('%a %e %b')
+
+class Location:
+    def __init__(self, location): # initialise class with name and coordinates
+        self.location = location
+        self.lattitude, self.longitude = get_coords(self.location)
+        self.weather_data = None
+        self.current_dt = None
+        self.current_weather_description = None
+        self.current_temp = None
+
+    def retrieve_weather(self): #send a get request to Openweather API to get weather data
+        lat = self.lattitude
+        long = self.longitude
+        weather_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={long}&exclude=minutely,hourly&appid={openweather_API_key}"
+        response = requests.get(weather_url)
+        status = response.status_code
+        if status == 200: # assign key data to object attributes
+            self.weather_data = response.json()
+            self.current_weather_description = self.weather_data['current']['weather'][0]['main'] 
+            self.current_dt = convert_dt(self.weather_data['current']['dt'],loc="header")
+            self.current_temp = round(self.weather_data['current']['temp']-273.15,1)
+        return status
+
+    def get_max_temp(self,day): # retrieve max temp depending on requested day
+        if self.weather_data:
+            return round(self.weather_data['daily'][day]['temp']['max']-273.15,1)
+        
+    def get_min_temp(self,day):# retrieve min temp depending on requested day
+        if self.weather_data:
+            return round(self.weather_data['daily'][day]['temp']['min']-273.15,1)
+        
+    def weather_description(self,day):# retrieve weather description depending on requested day
+        if self.weather_data:
+            return format_description(self.weather_data['daily'][day]['weather'][0]['description'])
+    
+    def get_dt(self,day):# retrieve date/time depending on requested day
+        if self.weather_data:
+            return convert_dt(self.weather_data['daily'][day]['dt'])
+
+
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String, nullable=False) 
+    answer = db.Column(db.String, nullable=False)
 
 # initialisation of chatbot
 my_bot = ChatBot(
@@ -126,10 +169,10 @@ def chat_window():
 
     return render_template('chat_window.html')
 
+
 @app.route('/get_weather',methods=['get','post'])
 def display_weather():
-    data_dict = {}
-    part = "minutely,hourly"
+    location_obj_list = []
 
     # if the user submits a new message, the chat continues
     #the user is redirected to the "chat_window" endpoint to maintain functionality
@@ -140,30 +183,29 @@ def display_weather():
         session['conversation'] = session['conversation']
         return redirect(url_for('chat_window'))
     
-    # error handling block for the API
     try:
-        #loop through each location and submit a GET request for each
-        for location in session['location']:
-            lat, long = get_coords(location) # retrieve coordinate for the API from the itinerary
-            weather_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={long}&exclude={part}&appid={openweather_API_key}"
-            response = requests.get(weather_url)
-            status = response.status_code
-            #store the API response in a dictionary
-            data_dict[location] = response.json()
-        
-        condition = data_dict[session['location'][0]]['current']['weather'][0]['main']
-        #get weather advice, dynamically add in name of user location
-        dict = weather_advice(session['location'][0])
-        #display advice by concatenating to bot response
-        bot_response = my_bot.get_response("user entered locations").text + f' {dict[condition]}'
+        #loop through each location 
+        for loc in session['location']:
+            location_obj = Location(loc) # instantiate new object for each location
+            status = location_obj.retrieve_weather() # call method to send GET request to Openweather
+            if status != 200: #error handling
+                raise Exception(f"Failed to retrieve weather data for {loc}. Status code: {status} ")
+            location_obj_list.append(location_obj) # list of location objects which are later utilised
+
+        weather_advice_list = []
+        for obj in location_obj_list:
+            # get weather advice from dictionary for each requested location
+            weather_advice_list.append(weather_advice(obj.location)[obj.current_weather_description])
+        bot_response = my_bot.get_response("user entered locations").text + f' {" ".join(weather_advice_list)}'
         session['conversation'].extend([bot_response])
         session['conversation'] = session['conversation']
 
-        #pass the dictionary storing the result alongside simple formatting functions to enhance display to user 
-        return render_template('weather_results.html', data_dict=data_dict, format_description=format_description, convert_dt=convert_dt)
-    except:
-        #display error message if there is an issue with the API
-        return f"There has been an error retrieving weather data. Status code: {status}"
+        #pass the location objects to the HTML template to display results
+        return render_template('weather_results.html', objs=location_obj_list)
+    
+    except Exception as e:
+        return f"There has been an error. {str(e)}"
+
 
 @app.route('/reset',methods=['post']) #route that handles the reset request
 def reset_chat():
